19cd44e637f17b8b773936aaea83ef7a6f118994
edamiee <damien.k.edwards@gmail.com>
2015-03-22 13:09:38 -0500

    Updated

--- ui.R
+++ ui.R
@@ -10,6 +10,13 @@ sidebarPanel(
   helpText("This data set contains statistics, in arrests per 100,000 residents for assault, murder, and rape in each of the 50 US states in 1973")
 ),
 mainPanel(
-  plotOutput('plot1')
+  plotOutput('plot1'),
+  
+  hr(),
+  h3("How to use Application"),
+  helpText(" Select the Violent Crime in the dropdown for Variable X to run k-means cluster"),
+  helpText(" Select the Violent Crime in the dropdown for Variable Y to run k-means cluster"),
+  helpText(" Click on the arrow(s) Up or Down to select the Cluster Count required"),
+  helpText(" Server will return Plot with a X over the Cluster of data based on the selected criteria")
 )
 ))
